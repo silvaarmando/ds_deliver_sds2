d43d352d59681e32a5ee5274fe73951be7015365
Francisco Armando Silva <franciscoarmando636@gmail.com>
2021-01-10 09:45:21 -0300

    GET /orders fineshed

---
+++ Backend/src/main/java/com/armandosouza/ds_deliver/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.armandosouza.ds_deliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.armandosouza.ds_deliver.dto.OrderDTO;
+import com.armandosouza.ds_deliver.entities.Order;
+import com.armandosouza.ds_deliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
